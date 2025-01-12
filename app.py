b8a42b3bc8b4c421015bd036637fe26864069aa6
Wiktor Nowicki <163419208+wsnowicki@users.noreply.github.com>
2025-01-12 17:50:52 +0100

    Dodanie linku do about

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from os import getenv
 
 # Wczytanie zmiennych z pliku .env
 load_dotenv()
-http_host = getenv("HTTP_HOST")
-http_port = getenv("HTTP_PORT")
-http_threads = getenv("HTTP_THREADS")
+HTTP_HOST = getenv("HTTP_HOST")
+HTTP_PORT = getenv("HTTP_PORT")
+HTTP_THREADS = getenv("HTTP_THREADS")
+SECRET_KEY = getenv('HTTP_SECRET')
 
 # Stworzenie aplikacji i nadanie tajnego klucza
 app = Flask(__name__)
-app.secret_key = 'super_tajny_klucz_ktory_napewno_nie_jest_udostepniony_w_repo_w_plain_textZZ'
+app.secret_key = SECRET_KEY
 
 # Przykładowa baza użytkowników
 users = {
@@ -55,10 +56,15 @@ def search():
             return render_template('EkranWyszukiwania.html', alert="Brak wyników")
     return render_template('EkranWyszukiwania.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('Blad.html'), 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host=http_host, port=http_port)
+    app.run(debug=True, host=HTTP_HOST, port=HTTP_PORT)
 
